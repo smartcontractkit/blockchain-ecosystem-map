7ca51aec2af8077641671980195650e89a222a3d
Mark Shenouda <mark@thisdot.co>
2022-03-31 15:38:51 +0200

    fix: scss modules for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,29 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-sass-postcss',
+    // nomodule
+    {
+      name: `@storybook/preset-scss`,
+      options: {
+        rule: {
+          test: /(?<!\.module).s[ca]ss$/,
+        },
+      },
+    },
+    // module
+    {
+      name: `@storybook/preset-scss`,
+      options: {
+        rule: {
+          test: /\.module\.s[ca]ss$/,
+        },
+        cssLoaderOptions: {
+          modules: {
+            localIdentName: '[name]__[local]--[hash:base64:5]',
+          },
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
