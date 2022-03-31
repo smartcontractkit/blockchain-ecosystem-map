887d718e7ff78a48094625beb682602c0e62321f
Mark Shenouda <mark@thisdot.co>
2022-03-31 14:28:21 +0200

    feat: install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-sass-postcss',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      type: 'json', // Required by Webpack v4
+      use: 'yaml-loader',
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, '../src/components'),
+      '@/data': path.resolve(__dirname, '../data'),
+      '@/pages': path.resolve(__dirname, '../src/pages'),
+      '@/helpers': path.resolve(__dirname, '../src/helpers'),
+      '@/styles': path.resolve(__dirname, '../src/styles'),
+      '/icons': path.resolve(__dirname, '../public/icons'),
+      '/logos': path.resolve(__dirname, '../public/logos'),
+    };
+    return config;
+  },
+};
