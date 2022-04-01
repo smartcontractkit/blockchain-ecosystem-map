60341730ba85e5fc5529d058afb8b91c8de99f40
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-01 15:06:54 +0100

    Typograghy in progress

---
+++ src/stories/Typography/Typography.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Typography() {
+  return (
+    <div>
+      <h2>Headings</h2>
+      <div>
+        <p className="heading heading__600">h600</p>
+        <small>.heading.heading__600</small>
+      </div>
+    </div>
+  );
+}
