d3d808a7cf96dacfefca3adbeb1e3b6e66894756
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-04-05 11:04:55 +0200

    feat: card (#39)
    * feat: card
    
    * feat: storybook

---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,17 @@
+import Card from './Card';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'Angular',
+  imageSrc: '/logos/angular.png',
+  url: 'https://www.google.com',
+  size: 'large',
+};
