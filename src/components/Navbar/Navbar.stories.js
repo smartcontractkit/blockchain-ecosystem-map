c5e585ea69eb0b4c9486784bc6b97c53c6bd0010
Mark Shenouda <mark@thisdot.co>
2022-04-06 14:02:29 +0200

    fix: navbar styles + functionality

---
+++ src/components/Navbar/Navbar.stories.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar';
+
+export default {
+  title: 'component/Navbar',
+  component: Navbar,
+};
+
+const Template = () => <Navbar />;
+
+export const Default = Template.bind({});
