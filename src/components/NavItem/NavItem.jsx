ad29a9951604ee9741d814bc3b1e6d8b532d1051
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-04-06 14:24:59 +0200

    Pr 7 - Navbar (#44)
    * feat: navbar
    
    * fix: svg currentColor
    
    * fix: navbar styles + functionality
    
    * fix: navbar a11y

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -4,18 +4,17 @@ import clsx from 'clsx';
 
 import styles from './NavItem.module.scss';
 
-export default function NavItem({ id, text, children, isSelected }) {
+export default function NavItem({ href, children, isSelected }) {
   const navItemClasses = clsx('text__short--md', styles.navItem, { [styles.selected]: isSelected });
   return (
-    <a href={`#${id}`} className={navItemClasses}>
-      {children} {text}
+    <a href={href} className={navItemClasses}>
+      {children}
     </a>
   );
 }
 
 NavItem.propTypes = {
-  id: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   isSelected: PropTypes.bool.isRequired,
 };
