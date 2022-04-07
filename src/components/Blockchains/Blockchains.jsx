6d00769927250200a7eac411cd5c060c251e0c29
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-07 14:59:39 +0100

    Feat: yaml file for blockchains, and section

---
+++ src/components/Blockchains/Blockchains.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from '@/components/Card';
+import blockchains from '@/data/blockchains.yaml';
+
+import styles from './Blockchains.module.scss';
+
+export default function Blockchains() {
+  return (
+    <section className={styles.container}>
+      <h2>Blockchains</h2>
+      <div className={styles.contents}>
+        {blockchains.map((blockchain, index) => (
+          <Card key={index} title={blockchain.name} imageSrc={blockchain.logo} url={blockchain.url} />
+        ))}
+      </div>
+    </section>
+  );
+}
