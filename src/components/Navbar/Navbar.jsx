0cd4fa9b179d0a7045303a48eb1bdf23a7c16875
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-08 11:50:43 +0100

    Progress bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,102 +13,142 @@ import MonitorIcon from '@/icons/monitor.svg';
 import ManageIcon from '@/icons/manage.svg';
 import CommunityIcon from '@/icons/community.svg';
 import MarketIcon from '@/icons/market.svg';
+import isElementVisible from '@/helpers/isElementVisible';
 
 function Navbar() {
   const [isScrollDown, setIsScrollDown] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [, setActiveSection] = useState(null); //this will be needed to know which navitem isselected
+
+  const updateProgress = (id) => {
+    const elm = document.querySelector('li#' + id);
+    if (elm) {
+      const nav = document.getElementById('nav');
+      const navwidth = nav.offsetWidth;
+      const left = Math.floor((elm.offsetLeft * 100) / navwidth);
+      const navitemId = id.split('-')[0];
+      console.log(navitemId);
+      setProgress(left + 5);
+      setActiveSection(navitemId);
+      return;
+    }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 200) {
+    const handleScroll = () => {
+      if (document.scrollY > 200) {
         setIsScrollDown(true);
-      } else if (window.scrollY < 50) {
+      } else if (document.scrollY < 50) {
         setIsScrollDown(false);
       }
-    });
+    };
+    document.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener('scroll');
+      document.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbar__group}>
-        {!isScrollDown && <h3>Get Started</h3>}
-        <ul>
-          <li>
-            <NavItem href="#learn">
-              <LearnIcon />
-              Learn
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#fund">
-              <FundIcon />
-              Fund
-            </NavItem>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.navbar__group}>
-        {!isScrollDown && <h3>Development Cycle</h3>}
-        <ul>
-          <li>
-            <NavItem href="#develop">
-              <DevelopIcon />
-              Develop
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#test">
-              <TestIcon />
-              Test
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#secure" isSelected>
-              <SecureIcon />
-              Secure
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#deploy">
-              <DeployIcon />
-              Deploy
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#monitor">
-              <MonitorIcon />
-              Monitor
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#manage">
-              <ManageIcon />
-              Manage
-            </NavItem>
-          </li>
-        </ul>
-      </div>
 
-      <div className={styles.navbar__group}>
-        {!isScrollDown && <h3>Share</h3>}
-        <ul>
-          <li>
-            <NavItem href="#community">
-              <CommunityIcon />
-              Community
-            </NavItem>
-          </li>
-          <li>
-            <NavItem href="#market">
-              <MarketIcon />
-              Market
-            </NavItem>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  useEffect(() => {
+    const sectionsElements = Array.from(document.querySelectorAll('section'));
+
+    const handleScroll = () => {
+      const activeSectionElement = sectionsElements.find((section) => isElementVisible(section));
+      if (activeSectionElement && activeSectionElement.id) {
+        let id = activeSectionElement.id;
+        updateProgress(`${id}-li`);
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={styles.container}>
+      <nav id="nav" className={styles.navbar}>
+        <div className={styles.navbar__group}>
+          {!isScrollDown && <h3>Get Started</h3>}
+          <ul>
+            <li id="learn-li">
+              <NavItem href="#learn">
+                <LearnIcon />
+                Learn
+              </NavItem>
+            </li>
+            <li id="fund-li">
+              <NavItem href="#fund">
+                <FundIcon />
+                Fund
+              </NavItem>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.navbar__group}>
+          {!isScrollDown && <h3>Development Cycle</h3>}
+          <ul>
+            <li id="develop-li">
+              <NavItem href="#develop">
+                <DevelopIcon />
+                Develop
+              </NavItem>
+            </li>
+            <li id="test-li">
+              <NavItem href="#test">
+                <TestIcon />
+                Test
+              </NavItem>
+            </li>
+            <li id="secure-li">
+              <NavItem href="#secure" isSelected>
+                <SecureIcon />
+                Secure
+              </NavItem>
+            </li>
+            <li id="deploy-li">
+              <NavItem href="#deploy">
+                <DeployIcon />
+                Deploy
+              </NavItem>
+            </li>
+            <li id="monitor-li">
+              <NavItem href="#monitor">
+                <MonitorIcon />
+                Monitor
+              </NavItem>
+            </li>
+            <li id="manage-li">
+              <NavItem href="#manage">
+                <ManageIcon />
+                Manage
+              </NavItem>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.navbar__group}>
+          {!isScrollDown && <h3>Share</h3>}
+          <ul>
+            <li id="community-li">
+              <NavItem href="#community">
+                <CommunityIcon />
+                Community
+              </NavItem>
+            </li>
+            <li id="market-li">
+              <NavItem href="#market">
+                <MarketIcon />
+                Market
+              </NavItem>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.progress} style={{ width: progress + '%' }}></div>
+      </nav>
+    </header>
   );
 }
 
