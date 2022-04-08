e290c82f8ba97916a06da6e83b09b5965e572444
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-08 09:42:10 +0100

    Pr 29/blockchain banner (#53)
    * Feat: yaml file for blockchains, and section
    
    * Feat: yaml file for blockchains, and section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Blockchains from '@/components/Blockchains';
 import Hero from '@/components/Hero';
 import styles from '@/styles/Home.module.scss';
 
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Hero />
+
+      <Blockchains />
     </div>
   );
 }
