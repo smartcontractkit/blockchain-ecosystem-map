ac728b163f1c2f8d337a25d61703f99e15bf0be8
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-12 13:09:21 +0100

    Fix: Restructured

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import Blockchains from '@/components/Blockchains';
 import Chapter from '@/components/Chapter';
 import Section from '@/components/Section';
@@ -12,14 +12,11 @@ import Card from '@/components/Card';
 export default function Home({ chapters }) {
   const [isExpanded, setisExpanded] = useState(null);
 
-  const { get_started, development_cycle, share } = chapters;
+  const chaptersKeys = Object.keys(chapters);
 
-  const expandPanel = useCallback(
-    (value) => {
-      setisExpanded(isExpanded && isExpanded === value ? null : value);
-    },
-    [isExpanded]
-  );
+  const expandPanel = (value) => {
+    setisExpanded(isExpanded && isExpanded === value ? null : value);
+  };
 
   return (
     <div className={styles.container}>
@@ -27,75 +24,29 @@ export default function Home({ chapters }) {
 
       <Blockchains />
 
-      {/* Get started */}
-      <Chapter>
-        {get_started.map((result) => (
-          <Section key={result.id} title={result.title} id={result.id}>
-            {result.data.map((data) => (
-              <InnerAccordion
-                key={data.id}
-                title={data.name}
-                id={data.id}
-                expanded={isExpanded === data.id}
-                expandToggle={expandPanel}
-              >
-                <div className={styles.accordion_contents}>
-                  {data.items.map((item) => (
-                    <Card key={item.url} title={item.title} imageSrc={item.logo} url={item.url} size="small" />
-                  ))}
-                </div>
-              </InnerAccordion>
-            ))}
-          </Section>
-        ))}
-      </Chapter>
-
-      {/* Development Cycle */}
-      <Chapter>
-        {development_cycle.map((result) => (
-          <Section key={result.id} title={result.title} id={result.title}>
-            {result.data.map((data) => (
-              <InnerAccordion
-                key={data.id}
-                title={data.name}
-                id={data.id}
-                expanded={isExpanded === data.id}
-                expandToggle={expandPanel}
-              >
-                <div className={styles.accordion_contents}>
-                  {data.items.map((item) => (
-                    <Card key={item.url} title={item.title} imageSrc={item.logo} url={item.url} size="small" />
-                  ))}
-                </div>
-              </InnerAccordion>
-            ))}
-          </Section>
-        ))}
-      </Chapter>
-
-      {/* Share */}
-
-      <Chapter>
-        {share.map((result) => (
-          <Section key={result.id} title={result.title} id={result.title}>
-            {result.data.map((data) => (
-              <InnerAccordion
-                key={data.id}
-                title={data.name}
-                id={data.id}
-                expanded={isExpanded === data.id}
-                expandToggle={expandPanel}
-              >
-                <div className={styles.accordion_contents}>
-                  {data.items.map((item) => (
-                    <Card key={item.url} title={item.title} imageSrc={item.logo} url={item.url} size="small" />
-                  ))}
-                </div>
-              </InnerAccordion>
-            ))}
-          </Section>
-        ))}
-      </Chapter>
+      {chaptersKeys.map((chapter, index) => (
+        <Chapter key={index}>
+          {chapters[chapter].map((result) => (
+            <Section key={result.id} title={result.title} id={result.id}>
+              {result.data.map((data) => (
+                <InnerAccordion
+                  key={data.id}
+                  title={data.name}
+                  id={data.id}
+                  expanded={isExpanded === data.id}
+                  expandToggle={expandPanel}
+                >
+                  <div className={styles.accordion_contents}>
+                    {data.items.map((item) => (
+                      <Card key={item.url} title={item.title} imageSrc={item.logo} url={item.url} size="small" />
+                    ))}
+                  </div>
+                </InnerAccordion>
+              ))}
+            </Section>
+          ))}
+        </Chapter>
+      ))}
     </div>
   );
 }
