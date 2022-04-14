ac5429d4dac297ac3f23f336f070387af89d5802
Mark Shenouda <mark@thisdot.co>
2022-04-14 13:37:36 +0200

    feat: Google analytics tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,11 @@ import Navbar from '@/components/Navbar';
 import chapters from '@/data/chapters';
 import '@/styles/globals.scss';
 import PropTypes from 'prop-types';
-
+import useGoogleTagManager from '@/helpers/useGoogleTagManager';
 function MyApp({ Component, pageProps }) {
   const [activelink] = useState('test');
+
+  useGoogleTagManager(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING);
 
   return (
     <>
