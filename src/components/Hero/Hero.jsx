7727399f24116ea96cdd013a18f62a957bc471e1
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-14 15:47:14 +0100

    Fix: Performance (#74)
    * Fix: Performance
    
    * fix: revert to img
    
    * fix: image quality

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
 import styles from './Hero.module.scss';
 
 function Hero() {
   return (
     <section className={styles.container}>
+      <Image
+        className={styles.image}
+        src={'/images/bg-hero.png'}
+        layout="fill"
+        objectFit="cover"
+        alt="hero image"
+        objectPosition="center"
+        priority
+        quality="100"
+      />
       <h1>Ecosystem Map</h1>
       <p>Everything you need to start building blockchain applications</p>
     </section>
