32237098d729785a6460cbbe6f8209ac92e1aa04
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-22 09:09:29 +0100

    Fix: aria-expanded dynamic

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -35,7 +35,7 @@ function Section({ title, id, children, Icon, expandToggle, expandedId }) {
   return (
     <div className={styles.container} role="region">
       <h3 id={id} ref={ref} className={styles.title}>
-        <button aria-expanded="true" aria-controls="sect3" onClick={toggle}>
+        <button aria-expanded={isOpen} aria-controls="sect3" onClick={toggle}>
           <ArrowDrop style={{ transform: isOpen ? 'rotate(90deg)' : 'rotate(0deg)' }} />
           {title}
         </button>
