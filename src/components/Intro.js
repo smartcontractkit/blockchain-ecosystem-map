1ffdb319d36978790d7dcd49f6c7838e53eee683
Jerry Hogan <jerry.hogan@thisdot.co>
2022-05-06 12:19:24 +0100

    in progress with responsive intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,27 +1,24 @@
 import { Steps } from 'intro.js-react';
 import { setCookie, parseCookies } from 'nookies';
+import { useEffect, useRef } from 'react';
 
-const steps = [
-  {
-    intro: 'Welcome to the Environment map for blockchain ecosystem!',
-  },
-  {
-    element: '#nav',
-    intro: 'This is your journey to learn how to create dApp',
-  },
-  {
-    element: '#learn-section',
-    intro: 'Each section has an expandable sections for you to explore',
-  },
-  {
-    element: '#blockchains-section',
-    intro: 'Here is your quick link to the most common blockchains',
-  },
-  {
-    element: '#github',
-    intro: 'The app is Opensource, so feel free to contribute too',
-  },
-];
+// const steps = [
+//   {
+//     intro: 'Welcome to the Environment map for blockchain ecosystem!',
+//   },
+//   {
+//     element: '#nav',
+//     intro: 'This is your journey to learn how to create dApp',
+//   },
+//   {
+//     element: '#learn-section',
+//     intro: 'Each section has an expandable sections for you to explore',
+//   },
+//   {
+//     element: '#blockchains-section',
+//     intro: 'Here is your quick link to the most common blockchains',
+//   }
+// ];
 
 const guideOptions = {
   nextLabel: 'Next',
@@ -29,6 +26,27 @@ const guideOptions = {
 };
 
 function Intro() {
+  const steps = useRef([
+    {
+      intro: 'Welcome to the Environment map for blockchain ecosystem!',
+    },
+    {
+      element: '#nav',
+      intro: 'This is your journey to learn how to create dApp',
+    },
+    {
+      element: '#learn-section',
+      intro: 'Each section has an expandable sections for you to explore',
+    },
+    {
+      element: '#blockchains-section',
+      intro: 'Here is your quick link to the most common blockchains',
+    },
+    {
+      element: '#github',
+      intro: 'The app is Opensource, so feel free to contribute too',
+    },
+  ]);
   const handleExit = () => {
     setCookie(null, 'hideTour', 'true', {
       maxAge: 9999 * 24 * 60 * 60,
@@ -38,8 +56,24 @@ function Intro() {
 
   const cookies = parseCookies();
 
+  useEffect(() => {
+    // if (window.screen < 1276) {
+    //   steps.current = steps.current.filter((step) => step.element !== '#github')
+    // }
+    //   window.addEventListener(
+    //     'resize',
+    //     function () {
+    //       /* In the future for responsiveness if needed we can check for screen width to know which rough value can be added */
+    //       if (window.screen < 1276) {
+    //         steps.current = steps.current.filter((step) => step.element !== '#github')
+    //       }
+    //     },
+    //     true
+    //   );
+  }, []);
+
   if (!cookies.hideTour)
-    return <Steps onExit={handleExit} steps={steps} enabled={true} initialStep={0} options={guideOptions} />;
+    return <Steps onExit={handleExit} steps={steps.current} enabled={true} initialStep={0} options={guideOptions} />;
 
   return null;
 }
