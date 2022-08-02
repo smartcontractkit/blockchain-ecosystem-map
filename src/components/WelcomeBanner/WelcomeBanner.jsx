b4e63fe63914881cd725ccff6c777ed1d78bafc2
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-02 16:21:29 +0100

    Welcome banner (#212)
    * in progress
    
    * in progress
    
    * banner in progress
    
    * chore: welcome banner

--- src/components/WelcomeBanner/WelcomeBanner.jsx
+++ src/components/WelcomeBanner/WelcomeBanner.jsx
@@ -1,5 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
 
+import styles from './WelcomeBanner.module.scss';
+
 export default function WelcomeBanner() {
-  return <div>WelcomeBanner</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <span className={styles.label}>smart contracts ecosystem</span>
+        <h1 className={styles.heading}>Everything you need to start developing blockchain apps</h1>
+      </div>
+
+      <div className={styles.content}>
+        <Link href="/map" passHref>
+          <a className={styles.get_started}>Get started</a>
+        </Link>
+        <div className={styles.overview}>
+          Explore the ecosystem map to get familiar with the landscape of <strong>hundreds of tools.</strong>
+        </div>
+      </div>
+    </div>
+  );
 }
