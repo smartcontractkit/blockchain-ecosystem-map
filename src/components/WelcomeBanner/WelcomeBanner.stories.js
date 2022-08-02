ffd3f57bbde4e472359d78775306a0ddd6bfb360
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-02 14:43:58 +0100

    Welcome page setup (#210)
    * in progress
    
    * in progress

---
+++ src/components/WelcomeBanner/WelcomeBanner.stories.js
@@ -0,0 +1,10 @@
+import WelcomeBanner from './WelcomeBanner';
+
+export default {
+  title: 'component/WelcomeBanner',
+  component: WelcomeBanner,
+};
+
+const Template = () => <WelcomeBanner />;
+
+export const Default = Template.bind({});
