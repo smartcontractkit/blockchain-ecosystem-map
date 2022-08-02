0a4ee333e79e2c02bd54d1313ac94de4df7eb476
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-02 13:43:54 +0100

    banner in progress

--- src/components/WelcomeBanner/WelcomeBanner.jsx
+++ src/components/WelcomeBanner/WelcomeBanner.jsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 
+import styles from './WelcomeBanner.module.scss';
+
 export default function WelcomeBanner() {
-  return <div>WelcomeBanner</div>;
+  return (
+    <div>
+      <h1 className={styles.heading}>Everything you need to start developing blockchain apps</h1>
+
+      <div className="content">
+        <Link href="/map" passHref>
+          <a className="get_started">Get started</a>
+        </Link>
+        <div className="overview">
+          Explore the ecosystem map to get familiar with the landscape of hundreds of tools.
+        </div>
+      </div>
+    </div>
+  );
 }
