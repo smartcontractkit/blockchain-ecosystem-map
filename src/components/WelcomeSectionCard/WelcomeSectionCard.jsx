84c6727f3d43841173c7eced4c62b6bae898c06f
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-03 17:00:12 +0100

    chore: cards created, responsive layout also

--- src/components/WelcomeSectionCard/WelcomeSectionCard.jsx
+++ src/components/WelcomeSectionCard/WelcomeSectionCard.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import styles from './WelcomeSectionCard.module.scss';
 // import clsx from 'clsx';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-export default function WelcomeSectionCard() {
-  return <div className={styles.wrapper}>WelcomeSectionCard</div>;
+export default function WelcomeSectionCard({ title, totalItem, children }) {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.info}>
+        <div className={styles.title}>
+          <span className={styles.icon}>{children}</span>
+          {title}
+        </div>
+        <div className={styles.count}>{totalItem}</div>
+      </div>
+      <div className={styles.card}></div>
+    </div>
+  );
 }
+
+WelcomeSectionCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  totalItem: PropTypes.number,
+  children: PropTypes.node,
+};
