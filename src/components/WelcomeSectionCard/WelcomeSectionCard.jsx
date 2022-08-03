f315e0096f70d3cb0684554897b0ea0472a28446
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-03 14:22:00 +0100

    card in progress

---
+++ src/components/WelcomeSectionCard/WelcomeSectionCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './WelcomeSectionCard.module.scss';
+// import clsx from 'clsx';
+// import PropTypes from 'prop-types';
+
+export default function WelcomeSectionCard() {
+  return <div className={styles.wrapper}>WelcomeSectionCard</div>;
+}
