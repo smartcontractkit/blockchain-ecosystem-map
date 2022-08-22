c1e1ffdd4fc54a4fe1560eb58d61d89b5682f2bd
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-22 14:25:33 +0100

    chore: hero updated and responsive (#251)
    * chore: hero updated and responsive
    
    * heading updated

--- src/components/WelcomeBanner/WelcomeBanner.jsx
+++ src/components/WelcomeBanner/WelcomeBanner.jsx
@@ -6,9 +6,9 @@ import styles from './WelcomeBanner.module.scss';
 export default function WelcomeBanner() {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.heading_container}>
         <span className={styles.label}>smart contracts ecosystem</span>
-        <h1 className={styles.heading}>Everything you need to start developing blockchain apps</h1>
+        <h1 className={styles.heading}>Everything you need to start building blockchaing apps</h1>
       </div>
 
       <div className={styles.content}>
@@ -19,7 +19,7 @@ export default function WelcomeBanner() {
           </a>
         </Link>
         <div className={styles.overview}>
-          Explore the ecosystem map to get familiar with the landscape of <strong>hundreds of tools.</strong>
+          Explore the ecosystem map to get familiar with the landscape of hundreds of tools.
         </div>
       </div>
     </div>
