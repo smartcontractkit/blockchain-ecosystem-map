84aaac486ef8f3fa1dd1b6bdc9296ab765f17c8a
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-22 14:24:50 +0100

    in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import WelcomeBanner from '@/components/WelcomeBanner';
-import WelcomeSection from '@/components/WelcomeSection';
 import styles from '@/styles/Home.module.scss';
 
 import chapters from '@/data/chapters';
@@ -9,20 +8,7 @@ import { countItems, getItems } from '@/helpers/getCardData';
 export default function Home() {
   const { get_started, development_cycle, share } = chapters;
 
-  const sections = [
-    {
-      tag: 'fundamentals',
-      content: get_started,
-    },
-    {
-      tag: 'development',
-      content: development_cycle,
-    },
-    {
-      tag: 'share',
-      content: share,
-    },
-  ];
+  const sections = [...get_started, ...development_cycle, ...share];
 
   return (
     <div className={styles.container}>
@@ -31,14 +17,10 @@ export default function Home() {
           <WelcomeBanner />
         </section>
         <section className={styles.contents}>
-          {sections.map(({ tag, content }, index) => (
-            <WelcomeSection key={index + tag} tag={tag}>
-              {content.map(({ id, title, data, Icon }) => (
-                <WelcomeSectionCard key={id} title={title} totalItem={countItems(data)} items={getItems(data)}>
-                  <Icon />
-                </WelcomeSectionCard>
-              ))}
-            </WelcomeSection>
+          {sections.map(({ id, title, data, Icon }, index) => (
+            <WelcomeSectionCard key={id + index} title={title} totalItem={countItems(data)} items={getItems(data)}>
+              <Icon />
+            </WelcomeSectionCard>
           ))}
         </section>
       </div>
