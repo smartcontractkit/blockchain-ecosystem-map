2966a40606bede867a35aade0fbd92e060ce1c89
Jerry Hogan <jerry.hogan@thisdot.co>
2022-09-27 09:12:32 +0100

    added url to .env

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,10 +3,11 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import styles from './Logo.module.scss';
 import Link from 'next/link';
+const { NEXT_BLOCKCHAIN_URL } = process.env;
 
 const Logo = ({ isHome, showBorder }) => {
   return (
-    <Link href="https://www.blockchain.education" passHref>
+    <Link href={NEXT_BLOCKCHAIN_URL || '#'} passHref>
       <a className={clsx(styles.logo, { [styles.map_page_logo]: !isHome, [styles.logo_border]: showBorder })}>
         <img src="/logo.svg" alt="blockchain logo" />
         <img src="/blockchain-logo-text.svg" alt="blockchain logo text" />
